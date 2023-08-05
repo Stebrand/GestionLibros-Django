d8b1ecd5780f496a02a2c410f4758edbf1e17baa
Stebrand <sbr16@hotmail.es>
2023-08-05 10:47:01 -0500

    ConecctionDatabase and First Migration

--- sistema/libreria/models.py
+++ sistema/libreria/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Libro(models.Model):
+    id= models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100, verbose_name='Titulo')
+    imagen = models.ImageField(upload_to='imaganes/',verbose_name='Imagen', null=True)
+    decription = models.TextField(verbose_name='Descripcion', null=True)
